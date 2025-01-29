93d4df44a98e14a91f6458eb6b6cb24ad16504bf
Avalondy <duyao2016@gmail.com>
2025-01-28 23:26:24 -0500

    Behavioural Patterns - Interpreter

---
+++ behavioural/src/bin/newtype.rs
@@ -0,0 +1,18 @@
+use std::fmt::Display;
+
+// Create Newtype Password to override the Display trait for String
+#[allow(dead_code)]
+struct Password(String);
+
+impl Display for Password {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "****************")
+    }
+}
+
+fn main() {
+    let unsecured_password: String = "ThisIsMyPassword".to_string();
+    let secured_password: Password = Password(unsecured_password.clone());
+    println!("unsecured_password: {unsecured_password}");
+    println!("secured_password: {secured_password}");
+}
